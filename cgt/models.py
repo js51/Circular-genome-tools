a73856919590a7e81310509821f112e6a2235f1d
Joshua Stevenson <joshuas153@Gmail.com>
2021-06-09 16:51:17 +1000

    new model

--- cgt/models.py
+++ cgt/models.py
@@ -7,6 +7,7 @@ from cgt import conversions, helper_functions
 class MODEL(Enum):
 	ONE_AND_TWO_REGION_INVERSIONS 	= auto()
 	TWO_REGION_INVERSIONS 			= auto()
+	ALL_INVERSIONS					= auto()
 
 # TODO: add the cuts function here, enable creation of more models
 # a model class which consists of a dictionary (?) of model elements mapping to their probability
@@ -35,8 +36,10 @@ def model(G, n, signed=True, model_type = MODEL.ONE_AND_TWO_REGION_INVERSIONS):
 		return set(sorted(__all_inversions_model(G, n, signed, num_regions = 2)))
 	elif model_type == MODEL.ONE_AND_TWO_REGION_INVERSIONS:
 		return set(sorted(__all_inversions_model(G, n, signed, num_regions = 1) + __all_inversions_model(G, n, signed, num_regions = 2)))
-	
-def __all_inversions_model(G, n, signed, num_regions):
+	elif model_type == MODEL.ALL_INVERSIONS:
+		
+
+def __all_inversions_model(G, n, signed, num_regions='any'):
 	if num_regions == 1:
 		if signed:
 			return [G(f'({i},-{i})') for i in range(1, n+1)]
@@ -47,5 +50,14 @@ def __all_inversions_model(G, n, signed, num_regions):
 			return [G(f'({i},{-1*(i+1)})({-1*i},{i+1})') for i in range(1,n)] + [G(f'({n},-1)(-{n},1)')]
 		else:
 			return [G(f'({i},{i+1})') for i in range(1,n)] + [G(f'(1,{n})')]
+	elif num_regions == 'any':
+		if signed: 
+			M = []
+			for permutation in H:
+				if set(permutation.cycle_type()) == {1,2} and cuts(conversions.cycles_to_signed_permutation(n, permutation), n) == 2:
+					M.append(permutation)
+			return permutation
+		else:
+			return 'not yet implemented'
 		
 	
