c6bb6956ecfe23da3a1527155f263d7c9b90c96c
Joshua Stevenson <joshuas153@Gmail.com>
2021-06-18 16:17:06 +1000

    add more conversion functions

--- cgt/position_paradigm.py
+++ cgt/position_paradigm.py
@@ -1,4 +1,5 @@
 from enum import Enum, auto
+import numpy as np
 
 class SYMMETRY(Enum):
     # Circular genomes
@@ -64,11 +65,18 @@ class PositionParadigmFramework:
             return self.genome_group()(element)
         except ValueError:
             pass # Not able to be directly converted, try something else!
-        if (not self.oriented) or len(element) == 2*self.n:
-            return self.genome_group()(element)
-        elif len(element) == self.n:
-            other_half = list(-int(element[self.n - i]) for i in range(1, self.n+1))
-            return self.genome_group()(element + other_half)
+        if type(element) is np.ndarray: # If it's a matrix, let's convert it!
+            return self.__permutation_group_element_from_matrix(element)
+        else: # Not a matrix, assume it's a list
+            try:
+                elt = list(element)
+            except TypeError:
+                raise ValueError("Invalid data for constructing a permutation")
+        if (not self.oriented) or len(elt) == 2*self.n:
+            return self.genome_group()(elt)
+        elif len(elt) == self.n:
+            other_half = list(-int(elt[self.n - i]) for i in range(1, self.n+1))
+            return self.genome_group()(elt + other_half)
         else:
             raise ValueError("Invalid data for constructing a permutation")
 
@@ -161,7 +169,7 @@ class PositionParadigmFramework:
             >>> PositionParadigmFramework(3).one_row('(1,-2)(-1,2)')
             [-2, -1, 3]
         """
-        elt = self.genome_group()(element)
+        elt = self.cycles(element)
         row = list(elt.dict().values())[0:self.n]
         if not as_list:
             if self.oriented:
@@ -169,6 +177,17 @@ class PositionParadigmFramework:
             else:
                 row = Permutations(self.n)(row)
         return row
+
+    def __permutation_group_element_from_matrix(self, matrix):
+        sigma = []
+        for c, col in enumerate(matrix.T):
+            image = np.nonzero(col)[0][0]
+            sigma.append(matrix[image,c]*(image+1))
+        return self.cycles(sigma)
+
+    def matrix(self, element):
+        """Return the defining representation matrix. Note that matrix(x)matrix(y) = matrix(yx)"""
+        return np.array(self.one_row(self.cycles(element)).to_matrix())
 
     def make_inversion(self, a, b):
         raise(NotImplementedError())
