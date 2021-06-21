fc7fe6fc9d0d1e48682b377dee7dba06a025344d
Joshua Stevenson <joshuas153@Gmail.com>
2021-06-22 09:49:00 +1000

    add distances module

---
+++ cgt/distances.py
@@ -0,0 +1,11 @@
+def mle(framework, model, genomes):
+    """Return dictionary of MLEs (if they exist) for time elapsed rearranging ref->genome for each genome"""
+    pass
+
+def min_distance(framework, model, genomes, weighted=False):
+    """Return dictionary of minimum distances ref->genome, using inverse of model probabilities as weights (or not)"""
+    pass
+
+def MFPT(framework, model, genomes, mean_inter_arrival_time=1):
+    """Return the mean time elapsed rearranging ref->genome where the target is an absorbing state"""
+    pass
