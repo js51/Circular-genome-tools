6e583c0fc3a4b781a8629a71ec1b02025761672d
Joshua Stevenson <joshuas153@Gmail.com>
2021-06-25 15:42:19 +1000

    add fix for gap memory issue

--- cgt/__init__.py
+++ cgt/__init__.py
@@ -1,7 +1,12 @@
 """
 """
-
 name = "cgt"
+from sage.all_cmdline import *
+gigs = 30
+old_command = gap._Expect__command
+s = old_command.index('-o ')
+e = old_command.index(' -', s)
+gap._Expect__command = gap._Expect__command.replace(gap._Expect__command[s:e], f'-o {gigs}G')
 from . import visualisation
 from . import models
 from . import position_paradigm
