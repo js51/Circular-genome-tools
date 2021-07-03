c8bc49db89f3a2f9bac835b48410da49e779390c
Joshua Stevenson <joshuas153@Gmail.com>
2021-07-03 19:09:15 +1000

    replace custom caching with functools cache, lru_cache

--- cgt/models.py
+++ cgt/models.py
@@ -4,6 +4,7 @@
 from sage.all_cmdline import QQ
 from .enums import *
 from . import rearrangements
+from functools import cache
 
 class Model:
     """Defines a model. A model consists of some collection of permutations and a map from these permutations to probabilities [0,1]"""
@@ -37,36 +38,27 @@ class Model:
                     model[generator] = relative_prob/len(gens)
         return cls(framework, model)
 
+    @cache
     def reg_rep_of_zs(self):
         """Return the regular representation of zs as comptued by PositionParadigmFramework.reg_rep_zs, but store the sparse result"""
-        try:
-            return self._reg_rep_of_zs
-        except AttributeError:
-            self._reg_rep_of_zs = self.framework.reg_rep_of_zs(self, sparse=True)
-            return self._reg_rep_of_zs
+        return self.framework.reg_rep_of_zs(self, sparse=True)
 
-    def generators(self):
-        return self.generating_dictionary
-
+    @cache
     def s_element(self, in_algebra=ALGEBRA.group):
         if in_algebra not in {ALGEBRA.group, ALGEBRA.genome}:
             raise NotImplementedError(f"Model element for {str(in_algebra)} algebra not yet implemented")
-        try:
-            return self.s[in_algebra]
-        except (AttributeError, KeyError):
-            A = self.framework.group_algebra()
-            s = A(0) # Zero element in algebra
-            gens_dict = self.generators()
-            gens = {x for x in self.generators().keys()}
-            while len(gens) > 0:
-                gen = gens.pop()
-                prob = gens_dict[gen]
-                if in_algebra is ALGEBRA.group:
-                    conj_class = rearrangements.conjugacy_class(self.framework, gen)
-                elif in_algebra is ALGEBRA.genome:
-                    conj_class = {gen}
-                for perm in conj_class:
-                    s += QQ(prob/len(conj_class)) * A(perm)
-                gens -= conj_class
-            self.s[in_algebra] = s
-            return s
+        A = self.framework.group_algebra()
+        s = A(0) # Zero element in algebra
+        gens_dict = self.generating_dictionary
+        gens = {x for x in self.generating_dictionary.keys()}
+        while len(gens) > 0:
+            gen = gens.pop()
+            prob = gens_dict[gen]
+            if in_algebra is ALGEBRA.group:
+                conj_class = rearrangements.conjugacy_class(self.framework, gen)
+            elif in_algebra is ALGEBRA.genome:
+                conj_class = {gen}
+            for perm in conj_class:
+                s += QQ(prob/len(conj_class)) * A(perm)
+            gens -= conj_class
+        return s
