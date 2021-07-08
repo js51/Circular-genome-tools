3186ff860bc34eb6200547556c0659f5362eac00
Joshua Stevenson <joshuas153@Gmail.com>
2021-07-08 15:40:33 +1000

    use group algebra s element

--- cgt/distances.py
+++ cgt/distances.py
@@ -18,7 +18,7 @@ def _projection_operators(mat, eigs):
 def _irreps_of_zs(framework, model, attempt_exact=False):
     CDF, UCF = ComplexDoubleField(), UniversalCyclotomicField()
     z = framework.symmetry_element()
-    s = model.s_element(in_algebra=ALGEBRA.genome)
+    s = model.s_element(in_algebra=ALGEBRA.group)
     irreps_of_z, irreps_of_s = framework.irreps(z), framework.irreps(s)
     irreps_of_zs = (matrix(UCF, irrep_z*irrep_s) for irrep_z, irrep_s in zip(irreps_of_z, irreps_of_s))
     irreps_of_zs = [Matrix(UCF if attempt_exact else CDF, irrep_zs) for irrep_zs in irreps_of_zs]
@@ -43,7 +43,6 @@ def _partial_traces_for_genome(framework, instance, irreps, irreps_of_zs, projec
         } for r in range(len(irreps_of_zs))
     }
     for r, irrep in enumerate(irreps): # Iterate over irreducible representations
-        print(f'\rComputing partial traces for irrep {r} of {len(irreps)}', end="")
         sigd = Matrix(CDF, matrix(UCF, irrep(framework.cycles(instance.inverse())))) * irreps_of_z[r]
         for e, eigenvalue in enumerate(eig_lists[r]):
             traces[r][eigenvalue] = round(real((sigd*projections[r][e]).trace()), 6)
