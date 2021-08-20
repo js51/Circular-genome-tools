02159d99ee8639ee990e2aea240c19e0156203d5
Joshua Stevenson <joshuas153@Gmail.com>
2021-08-20 12:16:32 +1000

    add binning and other routines (#33)

--- cgt/examples.py
+++ cgt/examples.py
@@ -7,7 +7,7 @@ from .position_paradigm import PositionParadigmFramework
 def example(n):
     framework = PositionParadigmFramework(n, symmetry=SYMMETRY.circular)
     model = Model.named_model_with_relative_probs(framework, {
-        MODEL.one_region_inversions: 0.5, 
-        MODEL.two_region_inversions: 0.5
+        MODEL.one_region_inversions: 2/3, 
+        MODEL.two_region_inversions: 1/3
     })
     return framework, model
