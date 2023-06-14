c7b4fdea025f7fb6033b32f5c9e6097af6edbea7
Joshua Stevenson <joshuas153@Gmail.com>
2023-06-14 18:59:57 +1000

    Create hyperoctahedral.py

---
+++ cgt/hyperoctahedral.py
@@ -0,0 +1,198 @@
+"""
+This file contains the class for the hyperoctahedral group.
+"""
+# %%
+from cgt import structures
+from sage.all_cmdline import (
+    SymmetricGroup,
+    cartesian_product,
+    GroupSemidirectProduct,
+    SymmetricGroupRepresentation,
+    SignedPermutations,
+    matrix,
+    block_matrix,
+    Partitions,
+)
+
+
+def HyperoctahedralGroupRepresentations(n):
+    """
+    This function returns the irreducible representations of the hyperoctahedral group.
+    """
+    irreps = {}
+    Hn = hyperoctahedral_group(n)
+    for little_subgroup_pair in Hn.little_subgroup_pairs():
+        for partition_left in Partitions(little_subgroup_pair[0]):
+            for partition_right in Partitions(little_subgroup_pair[1]):
+                partition_pair = (partition_left, partition_right)
+                rep = Hn.irrep(partition_pair)
+                irreps[partition_pair] = rep
+    return irreps
+
+
+class hyperoctahedral_group:
+    _n = None
+    _sage_signed_perms = None
+    _signed_perms = None
+    _subgroup_of_s2n = None
+    _semidirect_product = None
+    _Sn = None
+    _C2 = None
+    _C2n = None
+    _twist = None
+
+    def __init__(self, n, default_element_type="signed_permutation"):
+        self._n = n
+        self._singned_perms = structures.HyperoctahedralGroup(self._n)
+        self._sage_signed_perms = SignedPermutations(self._n)
+        self._Sn = SymmetricGroup(self._n)
+        self._C2 = SymmetricGroup(2)
+        self._C2n = cartesian_product(tuple(SymmetricGroup(2) for _ in range(self._n)))
+        self._semidirect_product = self.wreath_product_with_hyperoctahedral_twist(
+            self._C2n, self._Sn
+        )
+
+    def hyperoctahedral_twist(self, s, c):
+        try:
+            _ = len(s)  # works if it is a "tuple"
+        except:
+            pass
+        else:
+            s = self._Sn(s[0]) * self._Sn(s[1])
+        return self._C2n((c[s(k) - 1] for k in range(1, self._n + 1)))
+
+    def orbit_representative(self, l, m):
+        return self._C2n(tuple(() for _ in range(l)) + tuple((1, 2) for _ in range(m)))
+
+    def little_subgroup_pairs(self):
+        return tuple((l, self._n - l) for l in range(self._n + 1))
+
+    def little_subgroup(self, l, m, as_direct_product=False):
+        if l + m != self._n or l < 0 or m < 0 or self._n < 0:
+            raise ValueError("l,m must be non-negative and l+m must equal n")
+        if l == 0 or m == 0 and not as_direct_product:
+            return self._Sn
+        Sl = SymmetricGroup(list(range(1, l + 1)))
+        if as_direct_product:
+            Sm = SymmetricGroup(list(range(1, self._n - l + 1)))
+            return cartesian_product((Sl, Sm))
+        Sm = SymmetricGroup(list(range(l + 1, self._n + 1)))
+        return self._Sn.subgroup(Sl.gens() + Sm.gens())
+
+    def little_subgroup_pairs(self):
+        return [(l, self._n - l) for l in range(self._n + 1)]
+
+    def little_subgroups(self):
+        return [
+            self.little_subgroup(l, m) for l, m in self.little_subgroup_pairs(self._n)
+        ]
+
+    def orbit_representative(self, l, m):
+        return self._C2n(tuple(() for _ in range(l)) + tuple((1, 2) for _ in range(m)))
+
+    def character_for_tuple(self, c):
+        return tuple(
+            (lambda x: 1) if entry == self._C2(()) else (lambda x: x.sign())
+            for entry in c
+        )
+
+    def representation_little_subgroup(self, partition_pair):
+        rep_left = SymmetricGroupRepresentation(partition_pair[0])
+        rep_right = SymmetricGroupRepresentation(partition_pair[1])
+        l, m = sum([0] + list(partition_pair[0])), sum([0] + list(partition_pair[1]))
+        n = l + m
+        left_group = SymmetricGroup(l)
+        right_group = SymmetricGroup(m)
+        c = self.orbit_representative(l, m)
+        x = self.character_for_tuple(c)
+
+        def rep(g):
+            try:
+                _ = len(g[1])  # works if it is a "tuple"
+            except:  # g[1] is not a "tuple" of permutations
+                g = (g[0], self.element_in_little_subgroup(g[1], l, m))
+            prod = rep_left(g[1][0]).tensor_product(rep_right(g[1][1]))
+            character = (x[i](ci) for i, ci in enumerate(g[0]))
+            coeff = 1
+            for char in character:
+                coeff *= char
+            return coeff * prod
+
+        return rep
+
+    def irrep(self, partition_pair):
+        rep = self.representation_little_subgroup(partition_pair)
+        l, m = sum([0] + list(partition_pair[0])), sum([0] + list(partition_pair[1]))
+        subgroup = self.little_subgroup(l, m)
+        C2n_wr_SlxSm = self.wreath_product_with_hyperoctahedral_twist(
+            self._C2n, subgroup
+        )
+        tv = self.right_transversal(self._semidirect_product, C2n_wr_SlxSm)
+
+        def _irrep(g):
+            id_rep = rep(C2n_wr_SlxSm.one())
+            rep_dimension = (id_rep.nrows(), id_rep.ncols())
+            Y = [[None for _ in range(len(tv))] for _ in range(len(tv))]
+            for i in range(len(tv)):
+                for j in range(len(tv)):
+                    element = tv[i] * g * tv[j].inverse()
+                    if element in C2n_wr_SlxSm:
+                        Y[i][j] = rep(element)
+                    else:  # A matrix of zeros
+                        Y[i][j] = matrix(*rep_dimension)
+            return block_matrix(Y, subdivide=False)
+
+        return _irrep
+
+    def element_in_little_subgroup(self, elt, l, m):
+        """
+        Takes elt as an element of the little subgroup of Sn of order l!m! and returns an element of the direct product of S_l and S_m.
+        """
+        Sl = SymmetricGroup(l)
+        Sm = SymmetricGroup(m)
+        SlxSm = cartesian_product((Sl, Sm))
+        cycles = list(elt)
+        left = Sl.one()
+        right = Sm.one()
+        for cycle in cycles:
+            if cycle in Sl:
+                left *= Sl(cycle)
+            else:
+                string = str(cycle)
+                for new, old in enumerate(range(l + 1, self._n + 1)):
+                    string = string.replace(str(old), str(new + 1))
+                right *= Sm(string)
+        return SlxSm((left, right))
+
+    def wreath_product_with_hyperoctahedral_twist(self, H, G):
+        semidirect_product = GroupSemidirectProduct(
+            H,
+            G,
+            act_to_right=False,
+            twist=lambda s, c: self.hyperoctahedral_twist(s, c),
+        )
+
+        def random_element():
+            C, Sn = semidirect_product.cartesian_factors()
+            return semidirect_product((C.random_element(), Sn.random_element()))
+
+        def order():
+            return H.order() * G.order()
+
+        def elements():
+            return [semidirect_product((c, s)) for c in H for s in G]
+
+        semidirect_product.random_element = random_element
+        semidirect_product.action = self.hyperoctahedral_twist
+        semidirect_product.order = order
+        semidirect_product.elements = elements
+        return semidirect_product
+
+    def right_transversal(self, group, subgroup):
+        transversal = [subgroup.one()]
+        num_cosets = group.order() / subgroup.order()
+        while len(transversal) < num_cosets:
+            elt = group.random_element()
+            if all(elt.inverse() * t not in subgroup for t in transversal):
+                transversal.append(elt)
+        return transversal
