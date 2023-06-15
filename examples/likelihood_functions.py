0854e49709ddc3f7fcb26b6d2df10ddb95c495d4
Joshua Stevenson <joshuas153@Gmail.com>
2023-06-15 17:13:26 +1000

    move example files

--- examples/likelihood_functions.py
+++ examples/likelihood_functions.py
@@ -23,13 +23,11 @@ zs = s*z
 #%% 
 %%time
 
-from threading import Thread
-
 from cgt.distances import likelihood_function
 
 instances = [
     [1,2,3,4,  5,-6],
-    [1,2,-3,4, 5,-6],
+    [1,5,-3,4, 2,-6],
     [1,2,3,4,  5,-6],
     [1,2,-3,4, 5,-6],
 ]
@@ -38,24 +36,15 @@ instance_gen = map(framework.cycles, instances)
 dim = int(np.ceil(np.sqrt(len(instances))))
 fig, ax = plt.subplots(dim, dim, sharex=False, sharey=False)
 fig.tight_layout()
-threads = []
+
 for i in range(dim):
     for j in range(dim):
         try:
             instance = next(instance_gen)
         except StopIteration:
             break
-        def do_L(i,j):
-            L = likelihood_function(framework, model, instance, attempt_exact=False)
-            times = np.arange(0, 2100, 0.1)
-            ax[i, j].plot(times, [L(t) for t in times], color='black')
-        thread = Thread(target=do_L, args=(i,j))
-        thread.start()
-        threads.append(thread)
-
-for thread in threads:
-    thread.join()
-
-fig.show()
+        L = likelihood_function(framework, model, instance, attempt_exact=False)
+        times = np.arange(0, 25, 0.5)
+        ax[i, j].plot(times, [L(t) for t in times], color='black')
 
 # %%
