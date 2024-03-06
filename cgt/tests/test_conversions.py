a8c8feeb6471126fbc0878ce196933bd7de36330
Joshua Stevenson <joshuas153@gmail.com>
2024-03-06 13:35:22 +1100

    add tests

--- cgt/tests/test_conversions.py
+++ cgt/tests/test_conversions.py
@@ -3,7 +3,7 @@ import pytest
 
 @pytest.fixture
 def example_framework():
-    yield example(5)
+    yield example(10)
 
 def test_one_row_and_cycles_random(example_framework):
     fw, model = example_framework
@@ -14,3 +14,5 @@ def test_matrix_and_cycles_random(example_framework):
     fw, model = example_framework
     instance = fw.random_instance()
     assert instance == fw.cycles(fw.matrix(instance))
+
+def test_same_canonical_instance(example_framework)
