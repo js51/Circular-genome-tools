aeaa45e6c58ffd6436ade84349f9c60f1d9d92bb
Joshua Stevenson <joshuas153@gmail.com>
2024-05-29 13:04:55 +1000

    Fix test, version bump

--- cgt/tests/test_hyperoctahedral.py
+++ cgt/tests/test_hyperoctahedral.py
@@ -7,7 +7,7 @@ def test_hyperoctahedral_group_irreps():
         for irrep in framework.irreps():
             r1 = framework.random_instance()
             r2 = framework.random_instance()
-            assert (irrep(r1) * irrep(r2)) == irrep(r1*r2)
+            assert (irrep(r1) * irrep(r2)) == irrep(r2*r1)
 
 def test_hyperoctahedral_group_map():
     for n in (3, 4, 5, 10, 20, 30):
